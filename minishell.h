bdf50f7776b58045be7c8f798272862070c92d38
cviegas <cesairepro@gmail.com>
2024-03-08 16:05:11 +0100

    .h

--- minishell.h
+++ minishell.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 15:30:58 by legrandc          #+#    #+#             */
-/*   Updated: 2024/03/08 15:37:15 by legrandc         ###   ########.fr       */
+/*   Updated: 2024/03/08 16:04:37 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
+# include "includes/quoicoulibft/libft.h"
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
@@ -21,7 +21,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef struct s_vars
+typedef struct s_tokens
 {
 	t_tokens	*next;
 	char		*content;
