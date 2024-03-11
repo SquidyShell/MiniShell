473808c556165f40c2ae3593204b03cd3b9ce1a1
cviegas <cesairepro@gmail.com>
2024-03-11 17:37:13 +0100

    knows single_quote, let's delete theses

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:46:32 by cviegas           #+#    #+#             */
-/*   Updated: 2024/03/11 17:29:49 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/03/11 17:37:00 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,17 +51,14 @@ int	parsing(t_vars *v)
 	v->in_dquote = 0;
 	while (v->line[v->index])
 	{
-		if (v->line[v->index] && !v->in_quote)
+		if (v->line[v->index])
 		{
-			if (is_whitespace(v->line[v->index]))
-			{
-				if (tok_close(v) == -1)
-					return (-1);
-			}
-			else if (v->line[v->index] == '\'')
-				if_quote(v);
-			else if (get_type_and_len(v) == -1)
-				return (-1);
+			if (!v->in_quote && v->line[v->index] == '\'')
+				(case_word(v), v->in_quote = 1);
+			else if (v->in_quote && v->line[v->index] == '\'')
+				v->in_quote = 0;
+			if (!v->in_quote)
+				not_in_quote(v);
 		}
 		v->index++;
 	}
--- srcs/parsing/tokens_utils.c
+++ srcs/parsing/tokens_utils.c
@@ -6,7 +6,7 @@
 /*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 16:46:32 by cviegas           #+#    #+#             */
-/*   Updated: 2024/03/11 17:25:23 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/03/11 17:35:59 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,21 +26,6 @@ t_tokens	*tok_new(char *content, size_t type)
 	tok->error = false;
 	tok->is_double_quoted = 0;
 	tok->is_single_quoted = 1;
-	return (tok);
-}
-
-t_tokens	*tok_new_closed(char *content, size_t type)
-{
-	t_tokens	*tok;
-
-	tok = malloc(sizeof(t_tokens));
-	if (!tok)
-		return (NULL);
-	tok->content = content;
-	tok->type = type;
-	tok->closed = 1;
-	tok->next = NULL;
-	tok->error = false;
 	return (tok);
 }
 
