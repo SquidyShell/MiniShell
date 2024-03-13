9c35c30aa51c8c2e4fc39c7cdb21508fa2960e95
cviegas <cesairepro@gmail.com>
2024-03-13 22:35:52 +0100

    unset

--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -3,17 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   ft_unset.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 18:28:42 by legrandc          #+#    #+#             */
-/*   Updated: 2024/03/10 18:31:31 by legrandc         ###   ########.fr       */
+/*   Updated: 2024/03/13 22:34:29 by cviegas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	delete_var(t_list **env, bool first, t_list *prev_curr_next[3])
+{
+	if (first)
+	{
+		ft_lstdelone(prev_curr_next[1], free);
+		prev_curr_next[0]->next = prev_curr_next[2];
+	}
+	else
+	{
+		(*env) = (*env)->next;
+		ft_lstdelone(prev_curr_next[1], free);
+	}
+}
+
+void	delete_var_if_exists(t_list **env, char *arg)
+{
+	char	*content;
+	t_list	*next;
+	t_list	*previous;
+	t_list	*current;
+	bool	first;
+
+	first = 1;
+	previous = *env;
+	current = previous;
+	while (current)
+	{
+		next = current->next;
+		content = (char *)current->content;
+		if (!ft_strncmp(arg, content, ft_strlen(arg))
+			&& (content[ft_strlen(arg)] == '=' || !content[ft_strlen(arg)]))
+			return (delete_var(env, first, (t_list *[]){previous, current,
+					next}));
+		previous = previous->next;
+		current = previous->next;
+		if (first)
+			first = 0;
+	}
+}
+
 void	ft_unset(char **cmd, t_vars *vars)
 {
-	(void)cmd;
-	(void)vars;
+	size_t	i;
+
+	if (!cmd[0])
+		return ;
+	i = 0;
+	while (cmd[i])
+		delete_var_if_exists(&vars->env_list, cmd[i++]);
 }
