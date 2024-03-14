0dcaa35d7b4510e17e80ef671b743c476cf214fe
lgrandco <l.grandcoinlaf@gmail.com>
2024-03-14 15:57:50 +0100

    echo -n

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 18:27:41 by legrandc          #+#    #+#             */
-/*   Updated: 2024/03/14 01:08:53 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/03/14 15:57:06 by legrandc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static bool	is_option(char *cmd)
+{
+	if (*cmd++ != '-')
+		return (false);
+	while (*cmd)
+		if (*cmd++ != 'n')
+			return (false);
+	return (true);
+}
 
 void	ft_echo(char **cmd, t_vars *vars)
 {
@@ -19,21 +29,22 @@ void	ft_echo(char **cmd, t_vars *vars)
 
 	if (!cmd[1])
 		return (printfd(STDIN, "\n"));
-	print_endl = 0;
-	i = 1;
-	if (!ft_strcmp(cmd[1], "-n"))
+	print_endl = 1;
+	i = 0;
+	while (cmd[++i])
 	{
-		print_endl = 1;
-		i = 2;
+		print_endl = is_option(cmd[i]);
+		if (!print_endl)
+			break ;
 	}
 	while (cmd[i])
 	{
-		printfd(STDIN, "%s", cmd[i]);
+		printfd(STDOUT, "%s", cmd[i]);
 		if (cmd[i + 1])
-			printfd(STDIN, " ");
+			printfd(STDOUT, " ");
 		i++;
 	}
-	if (!print_endl)
-		printfd(STDIN, "\n");
+	if (print_endl)
+		printfd(STDOUT, "\n");
 	(void)vars;
 }
