c08e00f13ef135e0f4717f2e420d76e46fdf879a
lgrandco <l.grandcoinlaf@gmail.com>
2024-03-20 13:55:58 +0100

    fixed new comand after ctrl c in heredoc

--- srcs/init/init.c
+++ srcs/init/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cviegas <cviegas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: legrandc <legrandc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 21:40:22 by legrandc          #+#    #+#             */
-/*   Updated: 2024/03/20 12:07:56 by cviegas          ###   ########.fr       */
+/*   Updated: 2024/03/20 13:55:27 by legrandc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	init_vars(t_vars *v)
 	v->command_was_built_in = 0;
 	v->tokens = NULL;
 	v->cmd.args = NULL;
+	if (g_exit_status == 666)
+		g_exit_status = 130;
 }
 
 char	**ft_strdup_matrix(char **matrix)
